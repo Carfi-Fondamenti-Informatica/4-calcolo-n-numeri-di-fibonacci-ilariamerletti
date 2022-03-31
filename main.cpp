4020c3a4e2e25d5214ef3f3d1ecf8cf21e8d4ab9
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-03-31 16:36:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n;
+    int a = 1, b = 1, c, d = 3;
+    cin >> n;
+    if (n >= 2) {
+        cout << "1" << endl;
+        cout << "1" << endl;
+        while (n >= d) {
+            c = a + b;
+            cout << c << endl;
+            b = a;
+            a = c;
+            d = d + 1;
+        }
+    }
+       else if (n<2){
+           cout << "errore"<< endl;
+       }
+    return 0;
 }
